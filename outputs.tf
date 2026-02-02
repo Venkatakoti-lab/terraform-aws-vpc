04f7a80c8b0c11ce5fa0ea0ff5b5fd90bdd5d91f
KANAKAM VENKATA KOTAIAH <venkatakotaiahkanakam@gmail.com>
2026-02-02 19:30:14 +0530

    Modify outputs in outputs.tf
    Comment out existing outputs and add vpc_info output.

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,9 @@
-output "azs" {
-  value = data.aws_availability_zones.zones.names
+# output "azs_info" {
+#   value = slice(data.aws_availability_zones.available.names, 0,2)
+# }
+output "vpc_info" {
+  value = aws_vpc.main.id
 }
-output "vpc_id" {
-  value = aws_vpc.this.id
-}
-
-output "public_subnet_ids" {
-  value = aws_subnet.public[*].id
-}
-
-output "private_subnet_ids" {
-  value = aws_subnet.private[*].id
-}
-
-output "database_subnet_ids" {
-  value = aws_subnet.database[*].id
-}
-output "default_vpc_info" {
-  value = data.aws_vpc.default.id
-}
-output "default_vpc_cidr" {
-  value = data.aws_vpc.default.cidr_block
-}
+# output "default_vpc_id" {
+#   value = data.aws_vpc.default_vpc
+# }
